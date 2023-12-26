60adf20007cd119785c3dd3b82e774ce9e10c7ad
Mingyao Li <mingyao.li@plus.ai>
2023-12-26 15:41:11 +0800

    update small dataset

--- scripts/prepare_small_plus_data.sh
+++ scripts/prepare_small_plus_data.sh
@@ -4,8 +4,8 @@ source .venv2/bin/activate
 python tools/plus_data/download_plus.py\
     --lat=31.4720272\
     --lon=120.6019952\
-    --start_date="2023-09-01"\
-    --end_date="2023-10-31"\
+    --start_date="2023-07-01"\
+    --end_date="2023-08-31"\
     --start_hour=1\
     --end_hour=8\
     --vehicle_pattern="pdb-l4e-b" \
@@ -13,15 +13,28 @@ python tools/plus_data/download_plus.py\
     --snip_root="/mnt/bigfile_2/mingyao.li/nerf_data/selfocc_small/train"
 echo "train data downloaded"
 
-# valid
-python tools/plus_data/download_plus.py\
-    --lat=31.4720272\
-    --lon=120.6019952\
-    --start_date="2023-11-01"\
-    --end_date="2023-11-15"\
-    --start_hour=1\
-    --end_hour=8\
-    --vehicle_pattern="pdb-l4e-b" \
-    --do_snip=True\
-    --snip_root="/mnt/bigfile_2/mingyao.li/nerf_data/selfocc_small/valid"
-echo "valid data downloaded"
+
+#python tools/plus_data/download_plus.py\
+#    --lat=31.4720272\
+#    --lon=120.6019952\
+#    --start_date="2023-09-01"\
+#    --end_date="2023-10-31"\
+#    --start_hour=1\
+#    --end_hour=8\
+#    --vehicle_pattern="pdb-l4e-b" \
+#    --do_snip=True\
+#    --snip_root="/mnt/bigfile_2/mingyao.li/nerf_data/selfocc_small/train"
+#echo "train data downloaded"
+#
+## valid
+#python tools/plus_data/download_plus.py\
+#    --lat=31.4720272\
+#    --lon=120.6019952\
+#    --start_date="2023-11-01"\
+#    --end_date="2023-11-15"\
+#    --start_hour=1\
+#    --end_hour=8\
+#    --vehicle_pattern="pdb-l4e-b" \
+#    --do_snip=True\
+#    --snip_root="/mnt/bigfile_2/mingyao.li/nerf_data/selfocc_small/valid"
+#echo "valid data downloaded"
